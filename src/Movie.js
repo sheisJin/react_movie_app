78a072a48fe19ec98b6dfd8851d5a5240b7e3499
이진 <leejin1694@gmail.com>
2018-12-28 12:05:16 +0900

    리액트 기초 배우기 #13 functional

--- src/Movie.js
+++ src/Movie.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: propTypes.string.isRequired,
-        //isRequired 꼭 있어야 함.
-        poster: propTypes.string.isRequired
-
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePost poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePost poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    );
+}
+function MoviePost({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
 }
 
-class MoviePost extends Component {
-
-    static propTypes = {
-        poster: propTypes.string.isRequired
-    }
-
-    render(){
-        return (
-            <img src={this.props.poster} alt="Movie Poster" />
-        )
-    }
+Movie.propTypes = {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
 }
+MoviePost.propTypes = {
+    poster: propTypes.string.isRequired
+}
+
+
+
 
 export default Movie;
