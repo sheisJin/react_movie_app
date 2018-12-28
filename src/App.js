a167c5d60115762375f39b08129ba198a04012b9
이진 <leejin1694@gmail.com>
2018-12-28 15:57:38 +0900

    리액트 기초 배우기 #14 ajax on react

--- src/App.js
+++ src/App.js
@@ -11,29 +11,8 @@ class App extends Component {
   state = {
   }
 
-  componentDidMount(){  //did Mount 되었을때
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title: "Matrix",
-            poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
-          },
-          {
-            title: "full metal Jacket",
-            poster: "http://mblogthumb4.phinf.naver.net/20120612_175/taelove79_1339458744796e7jSI_JPEG/%B8%DE%C5%BB3.jpg?type=w2"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSztEwbh_k9ZSK0bSt1vxC25kbQ_Yg8To460wUddnPmtlH8rYTM"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-          }
-        ]
-      })
-    }, 5000)
+  componentDidMount() {
+    fetch('http://yts.ag/api/v2/list_movies.json?sort_by=rating')
   }
 
   _renderMovies = () => {
