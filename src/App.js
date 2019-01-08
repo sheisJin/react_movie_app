01c92a4785710b112efe4f6bcabde42923518d10
이진 <leejin1694@gmail.com>
2019-01-08 11:52:17 +0900

    리액트 기초 배우기 #16 Async Await

--- src/App.js
+++ src/App.js
@@ -12,15 +12,26 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://yts.ag/api/v2/list_movies.json?sort_by=rating')
+    this._getMovies();
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies // movies : movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('http://yts.ag/api/v2/list_movies.json?sort_by=rating')
     .then(potato => potato.json()) //potato 는 어떤 이름으로 해도 상관없다. console 에는 response 로 노출된다.
-    .then(json => console.log(json)) //fetch 에서 받은 파일을 json 형태로 받아오는것.
+    .then(json => json.data.movies) //fetch 에서 받은 파일을 json 형태로 받아오는것.
     .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie , index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_corver_image} key={movie.id} />
     })
     return movies
   }
